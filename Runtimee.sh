c92ef2a2b8bb88045f4f726c408830c505ab589c
god <godspeeeeed7@gmail.com>
2025-05-24 19:07:30 +0530

    started

--- Runtimee.sh
+++ Runtimee.sh
@@ -5,9 +5,9 @@ read INPUT1
 echo "INPUT2:"
 read INPUT2
 
-TIME =$(date)
-echo "Executed at: $TIME "
+TIMESTAMP=$(date)
+echo "Executed at: $TIMESTAMP "
 
-Sum =$(($INPUT1+$INPUT2))
+Sum=($INPUT1+$INPUT2)
 
-echo " sum of $INPUT1 and $INPUT2 is $Sum and executed at $time" 
+echo " sum of $INPUT1 and $INPUT2 is $Sum and executed at $TIMESTAMP" 
