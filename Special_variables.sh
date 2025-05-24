f39378ae566d447771f3185ac8fbfe6a6c2973a5
god <godspeeeeed7@gmail.com>
2025-05-24 22:49:04 +0530

    started

--- Special_variables.sh
+++ Special_variables.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Number of args: $#"
-echo "Current directory: $pwd "
+echo "Current directory: $PWD "
 echo "user directory: $HOME"
 echo "Script name: $0"
 echo "PID FOR SCRIPT :$$"
-echo "which user is running :$user"
+echo "which user is running :$USER"
 sleep 10 &
 echo "Pid for last background query :$!"
