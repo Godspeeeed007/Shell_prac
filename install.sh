fe366ee5bc89bb2e0b0ed044258a31f93e3e2abb
god <godspeeeeed7@gmail.com>
2025-05-24 23:52:46 +0530

    started

--- install.sh
+++ install.sh
@@ -7,8 +7,15 @@ if [ $USER -eq 0 ]
 then
     echo "You are running with root acess"
 
-    dnf module list mysql -y
-    
+    dnf list installed mysql -y
+    if [ $? -eq 0 ]
+    then 
+        echo "IT IS ALREADY THERE"
+        exit 0
+    else
+        echo "Not there...mysql is getting installed"
+        dnf install mysql -y
+        exit 0    
 else
     echo "Please run with root access"
     exit 1
