ad38aec2aa9b916ec0e5dce77b739e17c645f6b5
god <godspeeeeed7@gmail.com>
2025-05-25 10:51:52 +0530

    colurs and validation

--- install.sh
+++ install.sh
@@ -11,14 +11,13 @@ then
     if [ $? -eq 0 ]
     then 
         echo "IT IS ALREADY THERE"
-        exit 0
     else
         echo "Not there...mysql is getting installed"
         dnf install mysql -y 
         validate(){ 
         if [ $? -eq 0 ]
         then 
-        echo -e "\e[31m $2 is present"
+        echo -e "\e[32m $2 is present"
         else 
         echo -e "\e[31m $2 is not present"
         exit 1
@@ -32,7 +31,7 @@ then
         echo -e "\e[32m IT IS ALREADY THERE"
         exit 0
     else
-        echo -e "\e[31m Not there...nginx is getting installed"
+        echo -e "\e[31m Not there...nginx is getting installed]"
         dnf install nginx -y
         validate $? nginx   
     fi
