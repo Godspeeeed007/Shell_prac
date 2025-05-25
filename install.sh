37dda920d393bc2226d5866dd30df449f7be4558
god <godspeeeeed7@gmail.com>
2025-05-25 11:41:59 +0530

    colurs and validation

--- install.sh
+++ install.sh
@@ -5,22 +5,22 @@ USER=$(id -u)
 
 if [ $USER -eq 0 ]
 then
-    echo "You are running with root acess"
+    echo -e "\e[31mYou are running with root acess\e[0m""
 
     dnf list installed mysql 
     if [ $? -eq 0 ]
     then 
-        echo -e "\e[32m My sql installed \e[0m"
+        echo -e "\e[32mMysql installed\e[0m"
         dnf remove mysql -y
     else
-        echo  -e "\e[31m Not there...mysql is getting installed \e[0m"
+        echo  -e "\e[31mNot there...mysql is getting installed\e[0m"
         dnf install mysql -y 
         validate(){ 
         if [ $? -eq 0 ]
         then 
-        echo -e "\e[32m $2 Installed \e[0m"
+        echo -e "\e[32m$2 Installed\e[0m"
         else 
-        echo -e "\e[31m $2  Not Installed \e[0m"
+        echo -e "\e[31m$2  Not Installed\e[0m"
         exit 1
         fi
     }  
@@ -29,10 +29,10 @@ then
     dnf list installed nginx
     if [ $? -eq 0 ]
     then 
-        echo -e "\e[32m  Already Installed \e[0m"
+        echo -e "\e[32mAlready Installed\e[0m"
         dnf remove nginx -y
     else
-        echo -e "\e[31m nginx is not installed...nginx is getting installed \e[0m"
+        echo -e "\e[31mnginx is not installed...nginx is getting installed\e[0m"
         dnf install nginx -y
         validate $? nginx   
     fi
