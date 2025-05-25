e54e3e02eee350229fda9d252f174d8e3dec7daa
god <godspeeeeed7@gmail.com>
2025-05-25 10:25:27 +0530

    colurs and validation

--- install.sh
+++ install.sh
@@ -7,6 +7,16 @@ if [ $USER -eq 0 ]
 then
     echo "You are running with root acess"
 
+    validate(){ 
+        if[ $? -eq 0 ]
+        then 
+        echo-e "\e[31m $2 is present"
+        exit 1
+        else 
+        echo-e "\e[31m $2 is not present"
+        fi
+        }
+
     dnf list installed mysql 
     if [ $? -eq 0 ]
     then 
@@ -15,8 +25,20 @@ then
     else
         echo "Not there...mysql is getting installed"
         dnf install mysql -y
-        exit 0    
+        validate() $? mysql   
     fi
+
+    dnf list installed nginx
+    if [ $? -ne 0 ]
+    then 
+        echo -e "\e[32m IT IS ALREADY THERE"
+        exit 0
+    else
+        echo -e "\e[31m Not there...mysql is getting installed"
+        dnf install nginx -y
+        validate() $? nginx   
+    fi
+
 else
     echo "Please run with root access ex :sudo <file name>"
 
